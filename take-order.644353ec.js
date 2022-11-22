c3c26ca2c63b8e55607bbeb4c513bd9d8f6b1fc6
PrataCat <111517381+PrataCat@users.noreply.github.com>
2022-11-22 09:46:50 +0000

    Deploying to gh-pages from @ PrataCat/html-scc-team-03@7b7da4a94c093c5318e780be3dd7019b72a36033 🚀

---
+++ take-order.644353ec.js
@@ -0,0 +1,2 @@
+!function(){var e={mobMenuBtn:document.querySelector("[data-menu-btn]"),menu:document.querySelector("[data-menu]"),body:document.querySelector("body")};e.mobMenuBtn.addEventListener("click",(function(){e.mobMenuBtn.classList.toggle("is-open"),e.menu.classList.toggle("is-hidden"),e.body.classList.toggle("no-scroll")}))}();
+//# sourceMappingURL=take-order.644353ec.js.map
