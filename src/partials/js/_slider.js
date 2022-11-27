ec1fc8860916f407df74a61f32be7b9bacfac401
Hanna Ponomarenko <hannaponomarenko@ukr.net>
2022-11-27 22:14:40 +0200

    hover  & focus in header

--- src/partials/js/_slider.js
+++ src/partials/js/_slider.js
@@ -3,6 +3,9 @@ import Swiper, { Navigation, Pagination } from 'swiper';
 new Swiper('.swiper', {
   modules: [Navigation, Pagination],
   loop: true,
+  autoplay: {
+    delay: 3000,
+  },
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
