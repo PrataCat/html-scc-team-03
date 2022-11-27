77f058397ff1b1883008b2ca3fd42c0a21a245d1
Hanna Ponomarenko <hannaponomarenko@ukr.net>
2022-11-27 19:41:47 +0200

    https

--- src/partials/js/_slider.js
+++ src/partials/js/_slider.js
@@ -3,7 +3,6 @@ import Swiper, { Navigation, Pagination } from 'swiper';
 new Swiper('.swiper', {
   modules: [Navigation, Pagination],
   loop: true,
-  autoplay: { delay: 100 },
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
