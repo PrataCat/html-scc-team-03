2502680b9eb0b92ce3b1002455c034ef7d1b7239
Hanna Ponomarenko <hannaponomarenko@ukr.net>
2022-11-27 17:36:03 +0200

    add slider cycle

--- src/partials/js/_slider.js
+++ src/partials/js/_slider.js
@@ -2,6 +2,8 @@ import Swiper, { Navigation, Pagination } from 'swiper';
 
 new Swiper('.swiper', {
   modules: [Navigation, Pagination],
+  loop: true,
+  autoplay: { delay: 100 },
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
